0021e0fcc9ec74d601df3108f5d1ece830cc94b2
TeoVincent Artur Wincenciak <artur.wincenciak@gmail.com>
2024-12-22 13:09:33 +0100

    number

---
+++ section-01/13-number/file.js
@@ -0,0 +1,18 @@
+let textualNumber = '123'
+
+var concat = textualNumber + 2
+console.log(concat) // 1232 - It's a string concatenation
+// It is because of the addition operator, which implicitly casts the number to a string
+
+var number = parseInt(textualNumber)
+var sum = number + 2
+console.log(sum) // 125 - Now it's a number addition
+
+var product = textualNumber * 2
+console.log(product) // 246 - It's strange, isn't it?
+// It is because of the multiplication operator, which implicitly casts the string to a number
+
+// Try parse not a number
+let notANumber = 'abc'
+var parsed = parseInt(notANumber)
+console.log(parsed) // NaN - Not a Number
